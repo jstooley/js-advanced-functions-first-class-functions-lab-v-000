122c2bd49906677cd45ca9e1aa60a0b9329f3b5f
Justin Tooley <jstooley823@gmail.com>
2018-04-14 03:41:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ const fareDoubler = createFareMultiplier(2)
 
 const fareTripler = createFareMultiplier(3)
 
-const selectDifferentDrivers = function (drivers, fn){
-  return fn(drivers);
+const selectDifferentDrivers = (drivers,fn) => {
+  return fn(drivers)
 }
