f644716aa6f1bed0d0309950b12c82bbd3a11144
Justin Tooley <jstooley823@gmail.com>
2018-04-14 03:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function(drivers) {
+const returnFirstTwoDrivers = (drivers) => {
   var firstTwo = []
   for(let i = 0; i < 2; i++){
     firstTwo.push(drivers[i])
